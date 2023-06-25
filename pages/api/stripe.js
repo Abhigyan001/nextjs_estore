cc5a1ba1e18526b1fa5228613a50d0bbba29dbfc
Abhigyan001 <abhigyanmahanta001@gmail.com>
2023-06-25 12:14:36 -0400

    Fix checkout errors

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
-          { shipping_rate: 'shr_1NMvIkSGxecGSXGzznQtYHMP' },
+          { shipping_rate: 'shr_1NMvYOSGxecGSXGzpAKaXp1T' },
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
 
           return {
             price_data: {
-              currency: 'usd',
+              currency: 'inr',
               product_data: {
                 name: item.name,
                 images: [newImage],
